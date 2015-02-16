7cf6a5b270f9aa9b9a125d8bb2849b43921095d8
masbicudo <masbicudo@gmail.com>
2015-02-16 13:29:17 -0200

    Pager sample working and converted to a PagerSample.jsx component

--- samples/navigation/pager/pagerSample.jsx
+++ samples/navigation/pager/pagerSample.jsx
@@ -1,6 +1,7 @@
 var PagerSample = React.createClass({
     getInitialState: function () {
         return {
+            name: "xpto",
             pager1: {
                 page: 1,
                 pageSize: 10,
@@ -15,76 +16,43 @@ var PagerSample = React.createClass({
     },
     render: function () {
 
-        function onPagerChange(data, render) {
-            return function (e) {
-                e.preventDefault();
-                data[e.key] = e.value;
-                render();
-            }.bind(this);
-        };
-
         return (
             <div className="container">
 
                 <h1 className="page-header">pager.jsx</h1>
-                <h4>using external props (generic event)</h4>
 
+                <h4>using external props (generic event)</h4>
                 <Pager
                     page={this.state.pager1.page}
                     count={this.state.pager1.count}
                     pageSize={this.state.pager1.pageSize}
-                    onAnyChange={onPagerChange(this.state.pager1, this.forceUpdate() )} />
-
-
-
-                <div id="output1"></div>
-
-
+                    onAnyChange={function (e) {
+                            e.preventDefault();
+                            var newState = React.addons.update(
+                                this.state,
+                                { pager1: { page: { $set: e.value } }});
+                            this.setState(newState);
+                        }.bind(this)} />
 
                 <h4>using external props (specific event)</h4>
+                <Pager
+                    page={this.state.pager2.page}
+                    count={this.state.pager2.count}
+                    pageSize={this.state.pager2.pageSize}
+                    onPageChange={function (e) {
+                            e.preventDefault();
+                            var newState = React.addons.update(
+                                this.state,
+                                { pager2: { page: { $set: e.value } }});
+                            this.setState(newState);
+                        }.bind(this)} />
 
-                <div id="output2"></div>
                 <h4>using internal state (no events at all)</h4>
+                <Pager
+                    page={1}
+                    count={101}
+                    pageSize={10} />
 
-                <div id="output3"></div>
-                <script type="text/jsx">
-                    function onPagerChange(data, render) {
-                    return function (e) {
-                    e.preventDefault();
-                    data[e.key] = e.value;
-                    render();
-                    }.bind(this);
-                    }
-                    function render1() {
-                    React.render(
-                       ,
-                        document.getElementById("output1")
-                    );
-                    }
-                    function render2() {
-                    React.render(
-                        <Pager
-                            page={glb.pager2.page}
-                            count={glb.pager2.count}
-                            pageSize={glb.pager2.pageSize}
-                            onPageChange={onPagerChange(glb.pager2, render2)} />,
-                        document.getElementById("output2")
-                    );
-                    }
-                    function render3() {
-                    React.render(
-                        <Pager
-                            page={1}
-                            count={101}
-                            pageSize={10}
-                            render={render3} />,
-                        document.getElementById("output3")
-                    );
-                    }
-                    render1();
-                    render2();
-                    render3();
-                </script>
             </div>
         );
     }
