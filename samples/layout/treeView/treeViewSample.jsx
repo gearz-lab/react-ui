1183b4095bad82a0387ceff1c5f0fc2c0dc033f3
andrerpena <andrerpena@gmail.com>
2015-02-17 10:25:11 -0200

    Introducing the TreeView

---
+++ samples/layout/treeView/treeViewSample.jsx
@@ -0,0 +1,71 @@
+var TreeViewSample = React.createClass({
+    getInitialState: function () {
+        return {
+
+            treeView1: {
+                app: {
+                    display: "ApplicationControl",
+                    nodes: {
+                        page: {
+                            display: "PageControl",
+                            nodes: {
+                                editPanel: {
+                                    display: "StackPanelControl",
+                                    nodes: {
+                                        panel: {
+                                            display: "Panel (Main)",
+                                            nodes: {
+                                                name: {
+                                                    display: "TexboxControl (Name)"
+                                                },
+                                                dateOfBirth: {
+                                                    display: "DatePickerControl (Date of Birth)"
+                                                },
+                                                gender: {
+                                                    display: "ToggleButtonControl (Gender)"
+                                                }
+                                            }
+                                        },
+                                        panel2: {
+                                            display: "Panel (Additional Info)",
+                                            nodes: {
+                                                isResponsible: {
+                                                    display: "ToogleButtonControl (Is Responsible)"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        };
+    },
+    render: function () {
+
+        return (
+            <div className="container">
+
+                <h1 className="page-header">TreeView</h1>
+
+                <p>Displays hierarchical data</p>
+
+                { /* BASICS */ }
+                <h3>Basic usage</h3>
+                <p>Example of hierarchical data</p>
+
+                <CodeSample
+                    component={
+                        <TreeView data={this.state.treeView1} />
+                        }
+                    sourceCode= {{
+                        __html: ""
+                    }}
+                />
+            </div>
+        );
+    }
+});
---
+++ src/components/layout/treeView/treeView.jsx
@@ -0,0 +1,114 @@
+var TreeView = React.createClass({
+    mixins: [gearz],
+    propTypes: {
+        data: React.PropTypes.object.isRequired
+    },
+    getInitialState: function () {
+        return {}
+    },
+
+    traverseData: function (hierachicalData) {
+        var flatData = [];
+
+        var traverseInternal = function (currentKey, level, parentData, data) {
+            for (var prop in data) {
+                var newKey = currentKey ? currentKey + "." + prop : prop;
+                flatData.push({
+                    key: newKey,
+                    parentKey: currentKey,
+                    data: data[prop],
+                    childCount: data[prop].nodes ? Object.keys(data[prop].nodes).length : 0,
+                    level: level
+                });
+                traverseInternal(newKey, level + 1, data[prop], data[prop].nodes)
+            }
+        }
+
+        traverseInternal("", 0, null, hierachicalData);
+
+        return flatData;
+    },
+
+    updateState: function (key, collapsed) {
+        var keyParts = key.split(".");
+        var updateObject = {};
+        var targetObject = updateObject;
+        var originalObjectObject = updateObject;
+        for (var i = 0; i < keyParts.length; i++) {
+            updateObject[keyParts[i]] = {nodes: {}};
+            targetObject = updateObject[keyParts[i]];
+            updateObject = updateObject[keyParts[i]].nodes;
+        }
+        targetObject.collapsed = {$set: collapsed};
+        var newData = React.addons.update(this.get("data"), originalObjectObject);
+        this.set("data", newData);
+    },
+
+    render: function () {
+
+        var that = this;
+        var data = this.get("data");
+
+        var traversedData = this.traverseData(data);
+
+        // comnputes the paddinng-left of a node
+        var computeIdentMargin = function (node) {
+            var offset = 10;
+            return (offset + node.level * 15) + "px";
+        }
+
+
+        var getNode = function (key) {
+            for (var i = 0; i < traversedData.length; i++)
+                if (traversedData[i].key == key)
+                    return traversedData[i];
+            return null;
+        }
+
+        // determins whether or no the node is hidden
+        var isNodeHidden = function (node) {
+            var nodeParent = getNode(node.parentKey);
+            if (nodeParent == null)
+                return false;
+            if (nodeParent.data.collapsed == true)
+                return true;
+            return isNodeHidden(nodeParent)
+        }
+
+        // toggles the state of the given node
+        var toggleState = function (node) {
+            that.updateState(node.key, !node.data.collapsed);
+        }
+
+        return (
+            <ul className="list-group">
+            { traversedData.map(function (item) {
+                return isNodeHidden(item) ? null :
+                    <li className="list-group-item noselect" style={{paddingLeft: computeIdentMargin(item)}}>
+                        <span
+                            className={
+                                item.childCount ?
+                                (item.data.collapsed ? "treeView-toggle-button glyphicon glyphicon-triangle-right" : "treeView-toggle-button glyphicon glyphicon-triangle-bottom")
+                                    : "treeView-toggle-button glyphicon glyphicon-leaf"
+                                }
+                            onClick={ function () {
+                                toggleState(item)
+                            } } >
+                        </span>
+                        <span className="treeView-content">
+                            { item.data.display }
+                        </span>
+                        {
+                            item.childCount ?
+                                    <span className="badge">
+                                        { item.childCount }
+                                    </span>
+                                :
+                                null
+                        }
+                    </li>
+            }) }
+            </ul>
+        );
+    }
+});
