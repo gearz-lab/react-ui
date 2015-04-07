08058bfc123ed8741ef675b069f84f1719393aeb
andrerpena <andrerpena@gmail.com>
2015-04-07 17:49:36 -0300

    adding component factories

---
+++ src/lib/componentFactory/templates/stringTemplate.jsx
@@ -0,0 +1,9 @@
+var React = require("react");
+
+var StringTemplate = {
+    build: function(meta) {
+        return <Textbox id="something" name="something"></Textbox>;
+    }
+};
+
+module.exports = StringTemplate;
