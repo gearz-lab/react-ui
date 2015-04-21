0d67bc72689f7c8438d527476e264f538b039dd3
andrerpena <andrerpena@gmail.com>
2015-04-21 20:38:41 -0300

    Organizing the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,23 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({lazy: true});
 var colors = require('colors');
 var del = require('del');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
 
-gulp.task('clean-styles', function (done){
-    del(['./css/*.css', './css/*.map'], done);
-})
-gulp.task('styles', ['clean-styles'], function() {
+gulp.task('clean-css', function (){
+    del(['./css/*.css', './css/*.map']);
+});
+
+gulp.task('build-css', ['clean-css'], function() {
     return gulp.src('src/less/*.less')
         .pipe($.plumber())
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(minifyCSS())
-        .pipe(concat('react-ui.css'))
-        .pipe(sourcemaps.write('.'))
+        .pipe($.sourcemaps.init())
+        .pipe($.less())
+        .pipe($.minifyCss())
+        .pipe($.concat('react-ui.css'))
+        .pipe($.sourcemaps.write('.'))
         .pipe(gulp.dest('css'));
 });
 
-
-gulp.task('less-watcher', function() {
-    gulp.watch(['./css/*.css'], ['styles']);
+gulp.task('watch-css', function() {
+    // TODO: This is not deterministic... Sometimes it doesn't work. For precision, use 'build-css'
+    gulp.watch(['./src/less/*.less'], ['build-css']);
 })
